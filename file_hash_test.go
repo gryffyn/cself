0f9747461f55a7706e1a51aa73bca75043269a45
gryffyn <me@neveris.one>
2021-06-25 01:02:12 +0100

    fixed module change
    Signed-off-by: gryffyn <me@neveris.one>

--- file_hash_test.go
+++ file_hash_test.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/gryffyn/checksum"
+	"git.neveris.one/gryffyn/checksum"
 )
 
 func prepareFile() (string, error) {
--- hash_test.go
+++ hash_test.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gryffyn/checksum"
+	"git.neveris.one/gryffyn/checksum"
 )
 
 func TestSHA256sumReader(t *testing.T) {
